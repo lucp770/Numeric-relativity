d8366761580ee49825aa6442aeb2bb88208c813a
lucp770 <lucp770@gmail.com>
2020-03-21 10:44:11 -0300

    versão preliminar do programa

--- metricaS.c
+++ metricaS.c
@@ -1,7 +1,9 @@
+//importe das bibliotecas padrão
 #include <stdio.h>
 #include <stdlib.h>
 
 
+// método principal
 int main(){
 
 printf("%s\n \n \n", "Esse programa resolve numéricamente a métrica para o Buraco negro de Schwarzschild e compara o resultado com a sua solução analítica");
@@ -15,15 +17,16 @@ scanf("%f",&R);
 */
 
 double R =1 ; //teste inicial com o raio padrão 
-double r[1000];
+double r[1000];//define a matriz para armazenar os valores da distância radial onde o problema será resolvido
 
 //preenchendo o array r com os pontos 
 
-for(int i=1;i<1000;i=i++){
+for(int i=1;i<1000;i++){
 	r[i] = r[i-1]+0.1; //preenche o array r com incrementos de 0.1 a partir do raio do horizonte de eventos
 }
 
-printf("%s\n","Resolvendo numéricamente a métrica de Schwarzschild ...." );
+
+printf("%s\n \n ","Resolvendo numéricamente a métrica de Schwarzschild ....");
 
 //inserir aqui a solução pelo método das diferenças finitas
 
